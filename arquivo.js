986f87d29392b24edaed738e4940b602c8384ce2
alexafiliadodigital <108899496+alexafiliadodigital@users.noreply.github.com>
2023-08-27 19:05:20 -0300

    primeiro commit

---
+++ arquivo.js
@@ -0,0 +1,32 @@
+
+addEventListener("click",clicar)
+
+addEventListener("mouseout",sair)
+
+
+
+
+function clicar(){
+
+
+  var estilo=document.querySelector(".nav-link")
+  
+  estilo.style.backgroundColor="green"
+
+   
+
+
+}
+
+function sair(){
+
+  var mudar=document.querySelector(".nav-link")
+  mudar.style.backgroundColor="blue"
+
+}
+
+
+
+
+
+
